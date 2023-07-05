a121836a3fae516f420cc7fa575c4dd37a87fb74
Fabio Cotrim <fabiohenrique_p13@hotmail.com>
2023-07-05 17:06:43 -0300

    User update

--- src/main/java/com/cursoprogramacao/course/services/UserService.java
+++ src/main/java/com/cursoprogramacao/course/services/UserService.java
@@ -17,7 +17,6 @@ public class UserService {
 
 	public List<User> findAll() {
 		return repository.findAll();
-
 	}
 	
 	public User findById(Long id) {
@@ -32,5 +31,16 @@ public class UserService {
 	public void delete(Long id) {
 		repository.deleteById(id);
 	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getReferenceById(id);
+		updateData(entity, obj);
+		return repository.save(entity);		
+	}
 
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
+	}
 }
